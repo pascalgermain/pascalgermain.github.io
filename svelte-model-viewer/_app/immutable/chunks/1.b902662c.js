9cc350f9ace47049c3bc3cef35e477078fbd159a
Pascal Germain <pascalgermain@me.com>
2023-08-09 19:43:50 +0200

    Update svelte-model-viewer app

--- svelte-model-viewer/_app/immutable/chunks/1.b902662c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.f3b370c5.js";export{t as component};
--- svelte-model-viewer/_app/immutable/chunks/2.8e640081.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.51b4b7aa.js";export{t as component};
