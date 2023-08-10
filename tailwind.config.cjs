57d9b8aaf6db4f9ce822e7ea16cf90673368bff4
Pascal Germain <pascalgermain@me.com>
2023-08-10 16:39:34 +0200

    start index page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	corePlugins: {
+		ringWidth: false,
+	},
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: '#000',
+			white: '#fff',
+			gray: { 1: '#111', 2: '#222', 3: '#333', b: '#bbb' },
+			orange: { 1: '#b54901', 2: '#ed8133', 3: '#fe8f45' },
+		},
+		fontFamily: {
+			sans: ['Verdana', 'sans-serif'],
+		},
 		extend: {},
 	},
 	plugins: [],
